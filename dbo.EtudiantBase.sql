935c5007201c8a5bdc05bc446bfa802b389113e7
BrahimDlDiallo <165953017+BrahimDlDiallo@users.noreply.github.com>
2024-04-15 22:57:43 +0000

    Add files via upload

---
+++ dbo.EtudiantBase.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[EtudiantBase] (
+    [Id]             INT          IDENTITY (1, 1) NOT NULL,
+    [Nom]            VARCHAR (50) NULL,
+    [Prenom]         VARCHAR (50) NULL,
+    [TitreDeCours]   VARCHAR (50) NULL,
+    [NumeroCours]    VARCHAR (50) NULL,
+    [CodeCours]      INT          NULL,
+    [NoteCours]      DECIMAL (18) NULL,
+    [NumeroEtudiant] INT          NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
